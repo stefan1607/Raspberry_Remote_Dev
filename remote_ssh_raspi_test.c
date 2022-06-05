c5cbdc3ab36506fae7058bac5a4a8a1b76750c9b
stefan1607 <stefan.soyka@vodafone.de>
2022-06-05 12:07:31 +0200

    init01

---
+++ remote_ssh_raspi_test.c
@@ -0,0 +1,14 @@
+/*
+   *      p3-2.c
+   *      Beispielprogramm 2, Abschnitt 3
+   */
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t now;
+    now = time(0);
+    printf("Viel Spass nit C!!! \n");
+    return 0;
+
+}
