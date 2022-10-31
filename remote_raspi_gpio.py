328d3fd8487f0793e1bf415d353eba5259057fa2
stefan1607 <stefan.soyka@vodafone.de>
2022-10-31 14:56:42 +0100

    11

--- remote_raspi_gpio.py
+++ remote_raspi_gpio.py
@@ -4,9 +4,21 @@
 print ("Dies ist ein GPIO Test mit LEDs  siehe Raspberry PI - Kpmpendium seite 4224.")
 from gpiozero import LED
 from time import sleep
+
+# LED in connected to GPIO 18 port
 led = LED(18)
-led.blink(0.5, 0.5, background=True)
-print("Notice that control has moved away - hit Enter to continue")
-input()
-print("Control is now back")
-led.off()
+
+while True:
+    led.on()
+    sleep(0.5)
+    led.off()
+    sleep(0.5)
+
+
+#led.blink(0.5, 0.5, background=True)
+#print("Notice that control has moved away - hit Enter to continue")
+#input()
+#print("Control is now back")
+#led.off()
+
+print ("End of LED GPIO test")
