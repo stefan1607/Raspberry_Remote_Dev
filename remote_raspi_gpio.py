8138c28bab0b3654d92438a3a85820456296aa90
stefan1607 <stefan.soyka@vodafone.de>
2022-10-31 14:21:59 +0100

    2

--- remote_raspi_gpio.py
+++ remote_raspi_gpio.py
@@ -1,6 +1,12 @@
 # This is a successful test github remote + raspberry pi remote python development
 # including GitHub update and run/debug on raspberry pi
 
-print(3+5)
-print(2+4)
-print(1+1)
+print ("Dies ist ein GPIO Test mit LEDs  siehe Raspberry PI - Kpmpendium seite 4224.")
+from gpiozero import LED
+from time import sleep
+led = LED(18)
+led.blink(0.5, 0.5, background=True)
+print("Notice that control has moved away - hit Enter to continue")
+input()
+print("Control is now back")
+led.off()
