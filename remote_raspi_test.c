2a0cfcb862730c44768fc4ad6cf416501770c327
stefan1607 <stefan.soyka@vodafone.de>
2023-08-02 19:34:51 +0200

    2023-08-02

--- remote_raspi_test.c
+++ remote_raspi_test.c
@@ -4,12 +4,17 @@
    */
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 int main() {
     time_t seconds;
+    time_t twoseconds;
     seconds = time(0);
+    sleep(2);
+    twoseconds = time(0);
     printf("Have a lot of fun with C\n");
     printf("Seconds since January 1, 1970 = %ld\n", seconds);
+    printf("Seconds since January 1, 1970 = %ld\n", twoseconds);
     return 0;
 
 }
