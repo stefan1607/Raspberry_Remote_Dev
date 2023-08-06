042b4add600451fefcecbb33c11cb0500c24d58a
stefan1607 <stefan.soyka@vodafone.de>
2023-08-06 15:29:46 +0200

    bazel_init

--- test-c-prg/remote_raspi_test.c
+++ test-c-prg/remote_raspi_test.c
@@ -9,12 +9,16 @@
 int main() {
     time_t seconds;
     time_t twoseconds;
+    time_t threeseconds;
     seconds = time(0);
     sleep(2);
     twoseconds = time(0);
+    sleep(1);
+    threeseconds = time(0);
+
     printf("Have a lot of fun with C\n");
     printf("Seconds since January 1, 1970 = %ld\n", seconds);
     printf("Seconds since January 1, 1970 = %ld\n", twoseconds);
+    printf("Seconds since January 1, 1970 = %ld\n", threeseconds);
     return 0;
-
 }
