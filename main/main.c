3239d1548b90ae608eeed250fc4d3c8aee05c1cc
stefan1607 <stefan.soyka@vodafone.de>
2023-08-09 00:19:06 +0200

    bazel debug successful including libs

--- main/main.c
+++ main/main.c
@@ -4,9 +4,9 @@
    *      Title: RTC test program
    */
 
-// #include "main/main_2.h"
-// #include "lib/test_lib_1.h"
-// #include "lib/test_lib_2.h"
+#include "main/main_2.h"
+#include "lib/test_lib_1.h"
+#include "lib/test_lib_2.h"
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
@@ -16,9 +16,9 @@
 int main() {
     time_t seconds;
     time_t twoseconds;
-    // int resultAdd;
-    // int resultSub;
-    // int resultMul;
+    int resultAdd;
+    int resultSub;
+    int resultMul;
 
     seconds = time(0);
     sleep(2);
@@ -28,11 +28,11 @@ int main() {
     printf("Seconds since January 1, 1970 = %ld\n", twoseconds);
 
     printf("Some calculations\n");
-//    printf("Add: %d", add2integers(2,3));
+    printf("Add: %d", add2integers(2,3));
     printf("\n");
-//    printf("Sub: %d", sub2integers(3,2));
+    printf("Sub: %d", sub2integers(3,2));
     printf("\n");
-//    printf("Mul: %d", mul2integers(3,2));
+    printf("Mul: %d", mul2integers(3,2));
     printf("\n");
 
     return 0;
