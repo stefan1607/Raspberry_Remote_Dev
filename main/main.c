908b39a15d4328c017ae38119c978a49e96cdb75
stefan1607 <stefan.soyka@vodafone.de>
2023-10-14 13:39:58 +0200

    2023-10-14 I2C first try

--- main/main.c
+++ main/main.c
@@ -10,116 +10,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include <unistd.h>
 
 #include "/usr/local/include/wiringPi.h"
+#include "/usr/local/include/wiringPiI2C.h"
 #include "/usr/local/include/lcd.h"
+#include "/usr/local/include/bmp180.h"
 
 
-/*  This define is used for control the GPIO on my RaspberryPi 32bit.         */
-/*  Numbering of the LEDs you could find out via the command line tool GPIO   */
-/*  >gpio readall                                                             */
-/*  The column wPI is the one with the correct number                         */
+/*  This define is used for control the GPIO on my RaspberryPi 32bit.           */
+/*  Numbering of the LEDs you could find out via the command line tool GPIO     */
+/*  >gpio readall                                                               */
+/*  The column wPI is the one with the correct number                           */
 
-/*  Define data interface for humidity sensor DHT11 data pin */
+/*  Define device address for BMP180                                            */
+#define I2C_ADDRESS 0x77
+
+
+/*  Define data interface for humidity sensor DHT11 data pin                    */
 #define DHT_PIN 9 
-#define D4 2
-#define D5 3
+#define D4 5
+#define D5 6
 #define D6 17
 #define D7 27
 #define RS 22
 #define E 10
 
 
-int WaitEdge(int mode)
-{
-    int counter = 0;
-    while(digitalRead(DHT_PIN) == mode)
-    {
-        counter++;
-        delayMicroseconds(1);
-        if(counter == 255) return 1;
-    }
-    return 0;        
-}
-
-//
-// This function reads the temperature and humidity from DHT11
-// and returns the readings to the calling program
-//
-void Read_DHT11(int *T, int *H)
-{
-    char res, i, state, counter,indx = 0;
-    int data[5];
-
-    for(i = 0; i < 5; i++)data[i] = 0;
-    pinMode(DHT_PIN, OUTPUT);
-    digitalWrite(DHT_PIN, LOW);
-    delay(18);
-    digitalWrite(DHT_PIN, HIGH);
-    delayMicroseconds(40);
-    pinMode(DHT_PIN, INPUT);
-
-    res = WaitEdge(0);
-    res = res +WaitEdge(1);
-
-    i = 0;
-    while(i < 80 && res == 0)
-    {
-        counter = 0;
-        if(WaitEdge(0) == 1)break;
-        while(digitalRead(DHT_PIN) == HIGH)
-        {
-            counter++;
-            delayMicroseconds(1);
-            if(counter == 255)break;
-        }
-
-        data[indx/8] <<= 1;
-        if(counter > 28)data[indx/8] |= 1;
-          indx++;
-          i++;
-    }
-
-    // Check the Checksum //
-    if((indx >= 40) && (data[4] ==
-        ((data[0]+data[1]+data[2]+data[3]) & 0xFF)))
-    {
-        *T = data[2];   // T = data[2].data[3]
-        *H = data[0];   // H = data[0].data[1]
-    }
-}
 
 //
 // Start of MAIN program
 //
 int main(void)
 {
-    int Temp, Hum, lcd;
+    int Temp, Hum, lcd, bmp180, fd; 
 
     srand(time(NULL));
     wiringPiSetupGpio();
     delay(1000);
     lcd = lcdInit(2, 16, 4, RS, E, D4, D5, D6, D7, 0, 0, 0, 0);
 
-    while(1) 
-    {
-        Read_DHT11(&Temp, &Hum);
-        Temp = (rand() % 50) -10;
-        Hum = rand() % 100;
-        lcdClear(lcd);
-        lcdPosition(lcd, 0, 0);    // row 0, col 0
-        lcdPrintf(lcd, "Hallo Henry, ", 0);
-        lcdPosition(lcd, 0, 1);    // row 1, col 0
-        lcdPrintf(lcd, "ab ins Bett!", 1);
-        delay(5000);                // 5s delay
+    bmp180 = bmp180Setup (I2C_ADDRESS);
+
+
+    // Read_DHT11(&Temp, &Hum);
+    // Temp = (rand() % 50) -10;
+    // Hum = rand() % 100;
+        
+    lcdClear(lcd);
+    lcdPosition(lcd, 0, 0);    // row 0, col 0
+    lcdPrintf(lcd, "Hallo Henry, ", 0);
+    lcdPosition(lcd, 0, 1);    // row 1, col 0
+    lcdPrintf(lcd, "ab ins Bett!", 1);
+    delay(5000);                // 5s delay
     
-        lcdPosition(lcd, 0, 0);    // row 0, col 0
-        lcdPrintf(lcd, "T = %d C", Temp);
-        lcdPosition(lcd, 0, 1);     //row 1, col 0
-        lcdPrintf(lcd, "H = %d %%", Hum);
-        delay(5000);                // 5s delay
+    lcdPosition(lcd, 0, 0);    // row 0, col 0
+    lcdPrintf(lcd, "T = %d C", Temp);
+    lcdPosition(lcd, 0, 1);     //row 1, col 0
+    lcdPrintf(lcd, "H = %d %%", Hum);
+    delay(5000);                // 5s delay
+
+    
+    printf("BMP180 Test Program ...\n");
+    if(wiringPiSetup() < 0) return 1;
+    fd = wiringPiI2CSetup(0x77);
+  
+    load_calibration();
+    while(1)
+    {
+        printf("\nTemperature : %.2f C\n",read_temperature());
+        printf("Pressure :    %.2f Pa\n",read_pressure()/100.0);
+        printf("Altitude :    %.2f h\n",read_altitude());
+        delay(1000);
     }
+
+    return 0;
 
 }
 
